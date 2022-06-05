a7e5e1092b0c05b9655c00c1550084389d7bc628
ZachariahIdiculla <u17067210@tuks.co.za>
2022-06-05 18:50:15 +0200

    Created addOrder component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AddOrderComponent } from './add-order/add-order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddOrderComponent
   ],
   imports: [
     BrowserModule,

a8bb948bc80fa59d9b985c637e03dcbb82b4f06d
ZachariahIdiculla <u17067210@tuks.co.za>
2022-06-05 18:54:13 +0200

    add component to main html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AddOrderComponent } from './add-order/add-order.component';
 import { AddorderComponent } from './addorder/addorder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AddOrderComponent,
     AddorderComponent
   ],
   imports: [

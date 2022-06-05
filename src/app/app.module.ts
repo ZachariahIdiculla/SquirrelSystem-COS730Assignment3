63f45c795e0bc090b41335bfbfd5bb5abc38aade
ZachariahIdiculla <u17067210@tuks.co.za>
2022-06-05 18:52:25 +0200

    Created addorder
    last add order had a dash in-between which I did not like

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AddOrderComponent } from './add-order/add-order.component';
+import { AddorderComponent } from './addorder/addorder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AddOrderComponent
+    AddOrderComponent,
+    AddorderComponent
   ],
   imports: [
     BrowserModule,
